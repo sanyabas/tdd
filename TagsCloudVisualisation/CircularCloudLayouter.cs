6348647f43dd96a5549cc0b010f3a1dc24a6adc5
sanyabas <belyov97@gmail.com>
2016-10-20 12:04:07 +0500

    initial commit test with zero rectangle

---
+++ TagsCloudVisualisation/CircularCloudLayouter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualisation
+{
+    public class CircularCloudLayouter
+    {
+        private readonly Point center;
+        public CircularCloudLayouter(Point center)
+        {
+            this.center = center;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+            if (rectangleSize.Equals(new Size(0,0)))
+                return new Rectangle(1,0,0,0);
+            throw new NotImplementedException();
+        }
+
+    }
+
+    [TestFixture]
+    public class CircularCloudLayouter_Should
+    {
+        private CircularCloudLayouter layouter;
+        [SetUp]
+        public void SetUp()
+        {
+            layouter=new CircularCloudLayouter(new Point(0,0));
+        }
+
+        [Test]
+        public void PlaceAnywehe_ZeroRectangle()
+        {
+            var rectangle=layouter.PutNextRectangle(new Size(0, 0));
+            rectangle.Location.Should().NotBe(new Point(0, 0));
+        }
+    }
+}
